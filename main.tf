b0858970a4537fd72e551a7d35cad18afcf06b7e
Antonio Caravaca <antonio.caravaca@bi-geek.com>
2020-02-13 12:41:07 +0100

    Added SES topic subscription and updated HCL to 0.12+

--- main.tf
+++ main.tf
@@ -1,24 +1,38 @@
 resource "aws_ses_domain_identity" "ses_domain" {
-  domain = "${var.sesDomain}"
+  domain = var.sesDomain
 }
 
 resource "aws_ses_domain_dkim" "dkim_domain" {
-  domain = "${aws_ses_domain_identity.ses_domain.domain}"
+  domain = aws_ses_domain_identity.ses_domain.domain
 }
 
 resource "aws_route53_record" "ses_verification_record" {
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "_amazonses.${aws_ses_domain_identity.ses_domain.domain}"
   type    = "TXT"
   ttl     = "600"
-  records = ["${aws_ses_domain_identity.ses_domain.verification_token}"]
+  records = [aws_ses_domain_identity.ses_domain.verification_token]
 }
 
 resource "aws_route53_record" "dkim_verification_record" {
   count   = 3
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "${element(aws_ses_domain_dkim.dkim_domain.dkim_tokens, count.index)}._domainkey.${aws_ses_domain_identity.ses_domain.domain}"
   type    = "CNAME"
   ttl     = "600"
   records = ["${element(aws_ses_domain_dkim.dkim_domain.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
+resource "aws_ses_identity_notification_topic" "bounce_notification" {
+  topic_arn                = var.bounce_topic_arn
+  notification_type        = "Bounce"
+  identity                 = aws_ses_domain_identity.ses_domain.domain
+  include_original_headers = var.bounce_headers
+}
+
+resource "aws_ses_identity_notification_topic" "complaint_notification" {
+  topic_arn                = var.complaint_topic_arn
+  notification_type        = "Complaint"
+  identity                 = aws_ses_domain_identity.ses_domain.domain
+  include_original_headers = var.complaint_headers
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,41 @@
 variable "sesDomain" {
   description = "Domain for creating SES"
   default     = ""
-  type        = "string"
+  type        = string
 }
 
 variable "zone_id" {
   description = "ID from the Route53 zone where we're going to create the DNS records for SES"
   default     = ""
-  type        = "string"
+  type        = string
+}
+
+variable "bounce_topic_arn" {
+  description = "arn of SNS topic to subscribe"
+  default     = ""
+  type        = string
+}
+
+variable "complaint_topic_arn" {
+  description = "arn of SNS topic to subscribe"
+  default     = ""
+  type        = string
+}
+
+variable "bounce_headers" {
+  description = "Option to include original headers when sent to SNS"
+  default     = false
+  type        = bool
+}
+
+variable "complaint_headers" {
+  description = "Option to include original headers when sent to SNS"
+  default     = false
+  type        = bool
+}
+
+variable "notification_type" {
+  description = "Valid Values: Bounce, Complaint or Delivery"
+  default     = ""
+  type        = string
 }
